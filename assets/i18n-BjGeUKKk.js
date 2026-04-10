4e2141ae7bfa5accba227b6f0316f280f1da939b
blocklyscratch <hami_hsieh@hotmail.com>
2026-04-11 00:20:17 +0800

    update

---
+++ assets/i18n-BjGeUKKk.js
@@ -0,0 +1,129 @@
+import{d as N}from"./index-CuzsVzvN.js";import{c as x}from"./vue-i18n.runtime-DSNwFzed.js";const U={support:"Support",about:"About",paypal:{top:"Like ScratchJS? Support me via PayPal",coffee:"Have a cup of coffee",bottom:"Will be used for website/server maintenance, program development, and content updates. Thank you!"},singlePlayer:"Single Player",multiPlayer:"Multiplayer",recentProjects:"Recent Projects",winBadge:"Victory Badge",fastestScore:"Fastest Record",mainMenu:{explorerWeb:{title:"Explorer Web",subTitle:"Website/Reader/Editor",describe:`1. User-specific web pages, 
+ 2. Support file manager interface
+ 3. Search data`},explorerWebWinows:{title:"Explorer Web",subTitle:"executable file",describe:"1. Windows executable file"},markdown:{title:"Markdown",subTitle:"Reader/Editor",describe:`1. Easy to read and write, 
+ 2. Write explanatory documents or publish messages
+ 3. Convenient for A4 transfer and printing`},snowmenAttack:{title:"Snowman Attack",subTitle:"Single Player",describe:"Use programming blocks to control penguins and protect penguin eggs."},snowmenAttack18:{title:"Snowman Strikes L1~L8",subTitle:"Single-Player (How To)",describe:`Description: basic block control operation
+ condition: pass level 8
+ Reward badge: level 5 and level 8`},phaserWorld:{title:"Phaser World",subTitle:"Single Player",describe:`Description: Movable, explore the land.
+ Support: Star Grabbing Game
+ Reward Badge: None`},snowmenBadgeDescribe:{subTitle:"General Edition",describe:`Reward badge description and currently own badge
+`},plan:{phaserWorld:{title:"Phaser World",subTitle:"Multiplayer Edition",describe:`mobile, chat, explore the land.
+ multiplayer for grabbing stars `},snowmenAttack:{title:"Snowman Strikes",subTitle:"Battle Edition",describe:`Use programming blocks to fight.
+ Victory: chase away the snowman
+`}}},username:"Username",password:"password",repassword:"Type password again",login:"Login",loginFailed:"Login failed",register:"Register",forgot:"forgot password",allStatus:"All Status",pause:"Pause",resume:"Resume",all:"all",failed:"Error",success:"Success",disconnect:"Offline status",registerStatus:{ok:"Successful registration",error2:"Username already in use"},contact:{askingToJoin:"A request to join has been sent",waitingToAdd:"Waiting to add contacts"},phaser:{phaserNormalChat:"Character dialogue"},name:"name",Logic:"Logic",Loops:"Loop",Math:"Operation",Text:"Text",Lists:"List",Colour:"Color",Variables:"Variables",Functions:"Function",Penguin:"Penguin",Motion:"Motion",Sensing:"Detection",rePlay:"Play again",leveldown:"Level down",leveldown2:"Level down to level two",second:"second",badge:{share:"Click the image below to copy and share the link",levelJoin:"Welcome",level0:"Badge Name",level5:"Express Roadside Assistance",level8:"Speed ​​Roadside Assistance",jsChangeCount:"Update times",jsLinesCount:"Count of lines used",describe:"Badge description",yours:"Your Badge",describe0:"0: level/image",describeU:"U: Number of program changes, U1 -> 1 time",describeL:"L: number of blocks used, U6 -> 6",describeN:'(Click on "Badge" (left picture) to create a single web page for sharing)'},userOptions:{title:"Personal Options",ui:"User Locale",bUI:"Blockly Locale",restoreLayout:"Restore Layout"},type:{Number:"Number",Boolean:"Boolean",LogicBoolean:"Logic-Boolean",String:"string",Array:"Array",ArrayOfArray:"Array in Array",Colour:"Color"},snowmenAttack:{magicSealing:"Magic Seal",blockName:"Snowman Strikes",up:"Up",down:"down",throw:"throw a snowball",isThrowing:"Throwing snowballs?",getTrackID:"Which track?",getTrackIDCommet:`ex. Penguin returns 1 at the top 
+ The third returns 3`,snowmen:"Snowman",snowmenX:"[ ] snowmen percentage",enemyBallsArray:"Snowball percentage",enemyBallsArrayAllCommet:`[0,2,0,0] The value in the array is 0~8,
+As above, there are two snowballs in the second track`,theTrackID0:"First Track",theTrackID1:"Second track",theTrackID2:"The third track",theTrackID3:"Fourth Track",theTrackIDsnowmenCommet:`[] An array of detected yeti positions
+0~100. The larger the number, the closer to the penguin. 
+ [50] means it is in the middle
+ [30,50] means one is on the left and the other is in the middle
+ The input value is 0~3 (the array starts from 0)
+ 0 represents the first channel, 1 represents the second channel`,theTrackIDAll:"All Tracks",theTrackIDAllCommet:`[[First way][Second way][Third way][Fourth way]] The array in the array
+The value in the array is none or 0~100. The larger the number, the closer it is to the penguin. 
+ [50] means In the middle
+ [30,50] means one is on the left and one is in the middle`},backpackWindow:{userName:"My Backpack",snowmenName:"Snowmen Strike",defaultName:"Default Command",AddNew:{title:"Add to backpack",name:"name"}},hotkey:"Hotkey",describe:"Description",snowmenBackpack:{level6:{hasSnowmen3:{name:"The Third Snowman",describe:`Whether there is a snowman in the third lane,
+ click or press the shortcut key 2f , [Enter]`},throw3:{name:"The third throwing snowball",describe:`Throw a snowball at the third lane,
+ click or press the shortcut key 2t , [Enter]`}}},blocklyBackpack:{if:{name:"if",describe:"if ...etc"}},dialogClearWorkspace:{title:"Empty Program",describe:"All program blocks will be removed and cleared"},add:"Add",create:"Create",cancel:"Cancel",confirm:"Confirm",levelup:"upgrade",levelup30:"Upgrade (very easy to pass in 30 seconds)",levelup60:"Upgrade (very easy to pass in 60 seconds)",levelup90:"Upgrade (very easy to pass in 90 seconds)",dialogTargetItems:{title:"Level Quest"},start:"Start",fullscreen:"Full Screen",gameSetting:{fullscreenDescribe:"Click the icon on the left"},suggest:"Suggestion",nowDesktopOnly:"Mobile phone is not currently supported. (The screen is too small to apply) Thank you",restart:"Restart",pleaseWait:"Pause!!",turnOnBlockly:"Please turn on the Blockly program interface first",blocklyInterface:"Blockly Program Interface",currentFeature:"Currently all feature versions",level2:{defaultXml:{penguinis:"Penguin No. 1",road:"Road"}},comments:"Comments",mainPage:{mobilePhone:"Mobile phone (Portrait, multi-touch)",desktop:"Computer (Landscape)",howToDemo:"How to / Demo",howToProjects:{api:{title:"API - Weather API",subTitle:"",instructions:`
+
+Free weather API from https://api.open-meteo.com/v1/forecast?
+`}},recentMobile:{fireworks101:{title:"101 Fireworks",subTitle:"",instructions:`
+
+Referencing Gilbert and Wilbert's Scratch
+https://scratch.mit.edu/users/hp4703/
+
+https://scratch.mit.edu (CC BY-SA 2.0)
+
+Countdown Audio - 2025 Taipei 101 National Day Performance (Taipei 101 Official Channel)
+`},threeHeroes:{title:"Three Kingdoms Heroes",subTitle:"",instructions:`
+
+Refer to Gilbert and Wilbert's Scratch
+https://scratch.mit.edu/users/hp4703/
+
+https://scratch.mit.edu (CC BY-SA 2.0)
+
+`}}}},M={support:"支援",about:"關於",paypal:{top:"喜歡 ScratchJS 嗎？您可以透過 PayPal 支援我",coffee:"來杯咖啡",bottom:"您的支援將用於網站/伺服器維護、程式開發與內容更新。謝謝您！"},singlePlayer:"單人版",multiPlayer:"多人版",recentProjects:"近期專案",winBadge:"勝利徽章",fastestScore:"最快記錄",mainMenu:{explorerWeb:{title:"Explorer Web",subTitle:"網站/讀取/編輯器",describe:`1. 使用者個別網頁, 
+ 2. 支援檔案總管式操作介面
+ 3.搜尋資料`},explorerWebWinows:{title:"Explorer Web",subTitle:"執行檔",describe:"1. Windows 執行檔"},markdown:{title:"Markdown",subTitle:"讀取/編輯器",describe:`1. 易讀易寫, 
+ 2. 撰寫說明文件或發表訊息
+ 3.方便A4轉存列印`},snowmenAttack18:{title:"雪人來襲1~8關",subTitle:"單人版 (教學版)",describe:`內容說明: 基本程式積木控制操作與說明.
+ 勝利條件: 過第8關
+ 獎勵徽章: 第5關與第8關`},snowmenAttack:{title:"雪人來襲",subTitle:"單人版",describe:`內容說明: 使用程式積木控制企鵝保護企鵝蛋.
+ 勝利條件: 趕跑所有雪人
+ 獎勵徽章: 勝利徽章`},phaserWorld:{title:"飛梭世界",subTitle:"單人版",describe:`內容說明: 可移動,探險大地.
+ 目前支援: 搶星星遊戲
+ 獎勵徽章: 無`},snowmenBadgeDescribe:{subTitle:"通用版",describe:`獎勵徽章說明 與 目前擁有徽章
+`},plan:{phaserWorld:{title:"飛梭世界",subTitle:"多人版",describe:`內容說明: 可移動,聊天,探險大地.
+ 目前支援: 多人搶星星遊戲
+ 伺服器: `},snowmenAttack:{title:"雪人來襲",subTitle:"對戰版",describe:`內容說明: 使用程式積木對戰.
+ 勝利條件: 趕跑雪人得分計時賽
+`}}},username:"使用者名稱",password:"密碼",repassword:"請再次輸入密碼",login:"登入",loginFailed:"登入失敗",register:"註冊",forgot:"忘記密碼",allStatus:"所有數據",pause:"暫停",resume:"繼續",all:"全部",failed:"錯誤",success:"成功",disconnect:"離線狀態",registerStatus:{error2:"使用者名稱已被使用"},contact:{askingToJoin:"已傳送加入要求",waitingToAdd:"等待加入聯絡人"},phaser:{phaserNormalChat:"人物對話"},name:"名字",Logic:"邏輯",Loops:"循環",Math:"運算",Text:"文字",Lists:"列表",Colour:"顏色",Variables:"變數",Functions:"函式",Penguin:"企鵝",Motion:"動作",Sensing:"偵測",rePlay:"再玩一次",leveldown:"降一級",leveldown2:"降二級",second:"秒",badge:{share:"可點下圖複製分享連結",levelJoin:"歡迎加入",level0:"徽章名稱",level5:"快速道路救援",level8:"飆速道路救援",jsChangeCount:"更新次數",jsLinesCount:"使用行數",describe:"徽章說明",yours:"您的徽章",describe0:"0: 關卡/等級/圖像",describeU:"U: 程式更換次數, U1 -> 1次",describeL:"L: 使用積木數, U6 -> 6個",describeN:'(點選"徽章"(左圖)建立單一網頁, 可分享徽章)'},userOptions:{title:"個人選項",ui:"操作介面",bUI:"程式介面",restoreLayout:"復原版面"},type:{Number:"數字",Boolean:"布林",LogicBoolean:"邏輯-布林",String:"字串",Array:"陣列",ArrayOfArray:"陣列中陣列",Colour:"顏色"},snowmenAttack:{magicSealing:"魔法封印",blockName:"雪人來襲",up:"向上",down:"向下",throw:"丟雪球",isThrowing:"正在丟雪球?",getTrackID:"在第幾道?",getTrackIDAttack:"對方在第幾道?",getTrackIDCommet:`ex.企鵝在最上面傳回 1 
+ 第三道回傳 3`,snowmen:"雪人",snowmenX:"[ ] 雪人位置百分比",enemyBallsArray:"[ ] 雪球飛行百分比",enemyBallsArrayAllCommet:`[0,2,0,0] 陣列 中數值為 0~8 ,
+如上為第二道有二個雪球`,theTrackID0:"第一道",theTrackID1:"第二道",theTrackID2:"第三道",theTrackID3:"第四道",theTrackIDsnowmenCommet:`[] 每一個偵測到雪人位置的陣列
+0~100. 數字越大越靠近企鵝. 
+ [50]代表在中間
+ [30,50] 代表一隻在左方一隻在中間
+輸入數值為0~3 (陣列由0算起)
+ 0 代表第一道, 1代表第二道`,theTrackIDAll:"所有雪道",theTrackIDAllCommet:`[[第一道][第二道][第三道][第四道]] 陣列中的陣列
+  中數值為 沒有或 0~100. 數字越大越靠近企鵝. 
+ [50]代表在中間
+ [30,50] 代表一隻在左方一隻在中間 `},backpackWindow:{userName:"我的背包",snowmenName:"雪人來襲",defaultName:"預設指令",AddNew:{title:"加入背包",name:"名稱"}},hotkey:"快速鍵",describe:"說明",snowmenBackpack:{level6:{hasSnowmen3:{name:"第三道雪人",describe:`是否第三道有雪人,
+ 點選 或 按快速鍵 2f , [Enter]`},throw3:{name:"第三道丟雪球",describe:`到第三道丟雪球,
+ 點選 或 按快速鍵 2t , [Enter]`}}},blocklyBackpack:{if:{name:"如果",describe:"if ...etc"}},dialogClearWorkspace:{title:"清空程式",describe:"程式區塊會全部移除清空"},add:"新增",create:"創造",cancel:"取消",confirm:"確認",levelup:"升級",levelup30:"升級(很簡單 30秒就過關了)",levelup60:"升級(很簡單 60秒就過關了)",levelup90:"升級(很簡單 90秒就過關了)",dialogTargetItems:{title:"等級任務"},start:"開始",fullscreen:"全螢幕",gameSetting:{fullscreenDescribe:"點左圖示"},suggest:"建議",nowDesktopOnly:"手機暫不支援. (畫面太小不適用) 謝謝",restart:"重新開始",pleaseWait:"暫停!!",turnOnBlockly:"請先開啟 Blockly 程式介面",blocklyInterface:"Blockly 程式介面",currentFeature:"目前所有功能版",level2:{defaultXml:{penguinis:"企鵝在第",road:"道"}},comments:"評論",mainPage:{mobilePhone:"手機（直向螢幕、多點觸控操作)",desktop:"電腦(橫向螢幕）",howToDemo:"如何使用/展示",howToProjects:{api:{title:"API - 天氣 API",subTitle:"",instructions:`
+
+來自 https://api.open-meteo.com/v1/forecast? 的免費天氣 API
+`}},recentMobile:{fireworks101:{title:"101 煙火",subTitle:"",instructions:`參考 Gilbert 和 Wilbert 的 Scratch
+https://scratch.mit.edu/users/hp4703/
+
+https://scratch.mit.edu (CC BY-SA 2.0)
+
+倒數音源 - 2025台北101國慶展演 (台北101官方頻道)
+    `},threeHeroes:{title:"三國英雄",subTitle:"",instructions:`
+
+參考 Gilbert 和 Wilbert 的 Scratch
+https://scratch.mit.edu/users/hp4703/
+
+https://scratch.mit.edu (CC BY-SA 2.0)
+    `}}}},O={support:"支援",about:"关于",paypal:{top:"喜欢 ScratchJS 吗？您可以透过 PayPal 支援我",coffee:"来杯咖啡",bottom:"您的支援将用于网站/伺服器维护、程式开发与内容更新。谢谢您！ "},singlePlayer:"单人版",multiPlayer:"多人版",recentProjects:"近期专案",winBadge:"胜利徽章",fastestScore:"最快记录",mainMenu:{explorerWeb:{title:"Explorer Web",subTitle:"网站/读取/编辑器",describe:`1. 使用者个别网页, 
+ 2. 支援档案总管式操作介面
+ 3.搜寻资料`},explorerWebWinows:{title:"Explorer Web",subTitle:"执行档",describe:"1. Windows 执行档"},markdown:{title:"Markdown",subTitle:"读取/编辑器",describe:`1. 易读易写, 
+ 2. 撰写说明文件或发表讯息
+ 3.方便A4转存列印`},snowmenAttack18:{title:"雪人来袭1~8关",subTitle:"单人版 (教学版)",describe:`内容说明: 基本程式积木控制操作与说明.
+ 胜利条件: 过第8关
+ 奖励徽章: 第5关与第8关`},snowmenAttack:{title:"雪人来袭",subTitle:"单人版",describe:`内容说明: 使用程式积木控制企鹅保护企鹅蛋.
+ 胜利条件: 赶跑所有雪人
+ 奖励徽章: 胜利徽章`},phaserWorld:{title:"飞梭世界",subTitle:"单人版",describe:`内容说明: 可移动,探险大地.
+ 目前支援: 抢星星游戏
+ 奖励徽章: 无`},snowmenBadgeDescribe:{subTitle:"通用版",describe:`奖励徽章说明 与 目前拥有徽章
+`},plan:{phaserWorld:{title:"飞梭世界",subTitle:"多人版",describe:`内容说明: 可移动,聊天,探险大地.
+ 目前支援: 多人抢星星游戏
+ 伺服器: `},snowmenAttack:{title:"雪人来袭",subTitle:"对战版",describe:`内容说明: 使用程式积木对战.
+ 胜利条件: 赶跑雪人得分计时赛
+`}}},username:"使用者名称",password:"密码",repassword:"请再次输入密码",login:"登入",loginFailed:"登入失败",register:"注册",forgot:"忘记密码",allStatus:"所有数据",pause:"暂停",resume:"继续",all:"全部",failed:"错误",success:"成功",disconnect:"离线状态",registerStatus:{error2:"使用者名称已被使用"},contact:{askingToJoin:"已传送加入要求",waitingToAdd:"等待加入联络人"},phaser:{phaserNormalChat:"人物对话"},name:"名字",Logic:"逻辑",Loops:"循环",Math:"运算",Text:"文字",Lists:"列表",Colour:"颜色",Variables:"变数",Functions:"函式",Penguin:"企鹅",Motion:"动作",Sensing:"侦测",rePlay:"再玩一次",leveldown:"降一级",leveldown2:"降二级",second:"秒",badge:{share:"可点下图复制分享连结",levelJoin:"欢迎加入",level0:"徽章名称",level5:"快速道路救援",level8:"飙速道路救援",jsChangeCount:"更新次数",jsLinesCount:"使用行数",describe:"徽章说明",yours:"您的徽章",describe0:"0: 关卡/等级/图像",describeU:"U: 程式更换次数, U1 -> 1次",describeL:"L: 使用积木数, U6 -> 6个",describeN:'(点选"徽章"(左图)建立单一网页, 可分享徽章)'},userOptions:{title:"个人选项",ui:"操作介面",bUI:"程式介面",restoreLayout:"复原版面"},type:{Number:"数字",Boolean:"布林",LogicBoolean:"逻辑-布林",String:"字串",Array:"阵列",ArrayOfArray:"阵列中阵列",Colour:"颜色"},snowmenAttack:{magicSealing:"魔法封印",blockName:"雪人来袭",up:"向上",down:"向下",throw:"丢雪球",isThrowing:"正在丢雪球?",getTrackID:"在第几道?",getTrackIDCommet:`ex.企鹅在最上面传回 1 
+ 第三道回传 3`,snowmen:"雪人",snowmenX:"[ ] 雪人位置百分比",enemyBallsArray:"[ ] 雪球飞行百分比",enemyBallsArrayAllCommet:`[0,2,0,0] 阵列 中数值为 0~8 ,
+如上为第二道有二个雪球`,theTrackID0:"第一道",theTrackID1:"第二道",theTrackID2:"第三道",theTrackID3:"第四道",theTrackIDsnowmenCommet:`[] 每一个侦测到雪人位置的阵列
+0~100. 数字越大越靠近企鹅. 
+ [50]代表在中间
+ [30,50] 代表一只在左方一只在中间
+输入数值为0~3 (阵列由0算起)
+ 0 代表第一道, 1代表第二道`,theTrackIDAll:"所有雪道",theTrackIDAllCommet:`[[第一道][第二道][第三道][第四道]] 阵列中的阵列
+  中数值为没有或0~100. 数字越大越靠近企鹅. 
+ [50]代表在中间
+ [30,50] 代表一只在左方一只在中间`},backpackWindow:{userName:"我的背包",snowmenName:"雪人来袭",defaultName:"预设指令",AddNew:{title:"加入背包",name:"名称"}},hotkey:"快速键",describe:"说明",snowmenBackpack:{level6:{hasSnowmen3:{name:"第三道雪人",describe:`是否第三道有雪人,
+ 点选 或 按快速键 2f , [Enter]`},throw3:{name:"第三道丢雪球",describe:`到第三道丢雪球,
+ 点选 或 按快速键 2t , [Enter]`}}},blocklyBackpack:{if:{name:"如果",describe:"if ...etc"}},dialogClearWorkspace:{title:"清空程式",describe:"程式区块会全部移除清空"},add:"新增",create:"创造",cancel:"取消",confirm:"确认",levelup:"升级",levelup30:"升级(很简单 30秒就过关了)",levelup60:"升级(很简单 60秒就过关了)",levelup90:"升级(很简单 90秒就过关了)",dialogTargetItems:{title:"等级任务"},start:"开始",fullscreen:"全萤幕",gameSetting:{fullscreenDescribe:"点左图示"},suggest:"建议",nowDesktopOnly:"手机暂不支援. (画面太小不适用) 谢谢",restart:"重新开始",pleaseWait:"暂停!!",turnOnBlockly:"请先开启 Blockly 程式介面",blocklyInterface:"Blockly 程式介面",currentFeature:"目前所有功能版",level2:{defaultXml:{penguinis:"企鹅在第",road:"道"}},comments:"评论",mainPage:{mobilePhone:"手机（竖屏萤幕,多点触控操作)",desktop:"电脑(横屏萤幕）",howToDemo:"如何使用/展示",howToProjects:{api:{title:"API - 天气 API",subTitle:"",instructions:`
+
+来自 https://api.open-meteo.com/v1/forecast? 的免费天气 API
+`}},recentMobile:{fireworks101:{title:"101 烟火",subTitle:"",instructions:`
+
+参考 Gilbert 和 Wilbert 的 Scratch
+https://scratch.mit.edu/users/hp4703/
+
+https://scratch.mit.edu (CC BY-SA 2.0)
+
+倒数音源 - 2025台北101国庆展演 (台北101官方频道)
+`},threeHeroes:{title:"三国英雄",subTitle:"",instructions:`
+
+参考 Gilbert 和 Wilbert 的 Scratch
+https://scratch.mit.edu/users/hp4703/
+
+https://scratch.mit.edu (CC BY-SA 2.0)
+`}}}},F={"en-US":U,"zh-TW":M,"zh-CN":O},s={addEventListeners(r,e,o){for(let t=0,c=e.length;t<c;t++)r.addEventListener(e[t],o,{passive:!1})},removeEventListeners(r,e,o){for(let t=0,c=e.length;t<c;t++)r.removeEventListener(e[t],o,{passive:!1})},emitEvent:function(r,e,o){if(r.componentInstance)r.componentInstance.$emit(e,o);else{let t=new window.CustomEvent(e,{detail:o});r.el.dispatchEvent(t)}}},w=["mousedown","touchstart"],f=["mousemove","touchmove"],y=["mouseup","touchend"],v=function(r,e,o){let t=r,c=!0,p=window;typeof e.value=="boolean"?c=e.value:typeof e.value=="object"?(typeof e.value.target=="string"?(t=r.querySelector(e.value.target),t||console.error("There is no element with the current target value.")):typeof e.value.target<"u"&&console.error(`The parameter "target" should be either 'undefined' or 'string'.`),typeof e.value.container=="string"?(p=document.querySelector(e.value.container),p||console.error("There is no element with the current container value.")):typeof e.value.container<"u"&&console.error(`The parameter "container" should be be either 'undefined' or 'string'.`),typeof e.value.active=="boolean"?c=e.value.active:typeof e.value.active<"u"&&console.error(`The parameter "active" value should be either 'undefined', 'true' or 'false'.`)):typeof e.value<"u"&&console.error("The passed value should be either 'undefined', 'true' or 'false' or 'object'.");const k=function(u,m){p===window?window.scrollBy(u,m):(p.scrollLeft+=u,p.scrollTop+=m)},T=function(){let u,m,b,h=!1;t.md=function(n){const d=n instanceof window.MouseEvent,a=d?n.pageX:n.touches[0].pageX,i=d?n.pageY:n.touches[0].pageY,l=document.elementFromPoint(a-window.pageXOffset,i-window.pageYOffset),g=e.arg==="nochilddrag",A=e.modifiers.noleft,C=e.modifiers.noright,L=e.modifiers.nomiddle,P=e.modifiers.noback,I=e.modifiers.noforward,W=e.arg==="firstchilddrag",B=l===t,D=l===t.firstChild,E=g?typeof l?.dataset.dragscroll<"u":typeof l?.dataset.noDragscroll>"u";if(!(!B&&(!E||W&&!D))&&!(n.button===0&&A)){if(n.button===1&&L||n.button===2&&C||n.button===3&&P||n.button===4&&I)return;b=1,u=d?n.clientX:n.touches[0].clientX,m=d?n.clientY:n.touches[0].clientY}},t.mu=function(n){b=0,h&&s.emitEvent(o,"dragscrollend"),h=!1},t.mm=function(n){const d=n instanceof window.MouseEvent;let a,i;if(b){n.preventDefault(),h||s.emitEvent(o,"dragscrollstart"),h=!0;const l=t.scrollLeft+t.clientWidth>=t.scrollWidth||t.scrollLeft===0,g=t.scrollTop+t.clientHeight>=t.scrollHeight||t.scrollTop===0;a=-u+(u=d?n.clientX:n.touches[0].clientX),i=-m+(m=d?n.clientY:n.touches[0].clientY),e.modifiers.pass?(t.scrollLeft-=e.modifiers.y?-0:a,t.scrollTop-=e.modifiers.x?-0:i,t===document.body&&(t.scrollLeft-=e.modifiers.y?-0:a,t.scrollTop-=e.modifiers.x?-0:i),(l||e.modifiers.y)&&k(-a,0),(g||e.modifiers.x)&&k(0,-i)):(e.modifiers.x&&(i=-0),e.modifiers.y&&(a=-0),t.scrollLeft-=a,t.scrollTop-=i,t===document.body&&(t.scrollLeft-=a,t.scrollTop-=i)),s.emitEvent(o,"dragscrollmove",{deltaX:-a,deltaY:-i})}},s.addEventListeners(t,w,t.md),s.addEventListeners(window,y,t.mu),s.addEventListeners(window,f,t.mm)};c?document.readyState==="complete"?T():window.addEventListener("load",T):(s.removeEventListeners(t,w,t.md),s.removeEventListeners(window,y,t.mu),s.removeEventListeners(window,f,t.mm))},j=r=>{const e=r;s.removeEventListeners(e,w,e.md),s.removeEventListeners(window,y,e.mu),s.removeEventListeners(window,f,e.mm)},S={mounted:(r,e,o)=>v(r,e,o),updated:(r,e,o)=>{JSON.stringify(e.value)!==JSON.stringify(e.oldValue)&&v(r,e,o)},unmounted:r=>j(r)},R={install(r){r.directive("dragscroll",S)}};typeof window<"u"&&window.Vue&&(window.VueDragscroll=S);const J=N(({app:r})=>{const e=x({locale:"en-US",globalInjection:!0,messages:F});r.use(e),r.use(R)});export{J as default};
