58cf39af6ca5b64d738e2a99b10ea7de0f32ba70
blocklyscratch <hami_hsieh@hotmail.com>
2026-07-26 00:48:48 +0800

    update

--- go/assets/vue-i18n.runtime-BKIxf2x8.js
+++ go/assets/vue-i18n.runtime-BKIxf2x8.js
@@ -1,4 +1,4 @@
-import{ak as In,r as yn,al as Sn,b as Le,w as tt,am as On,an as ft,h as Qt,g as qt,ae as Zt,I as kn,k as Pn,m as An,$ as vn,ao as Rn}from"./index-Bu8rNhE4.js";/*!
+import{ak as In,r as yn,al as Sn,b as Le,w as tt,am as On,an as ft,h as Qt,g as qt,ae as Zt,I as kn,k as Pn,m as An,$ as vn,ao as Rn}from"./index-DGEr6yUv.js";/*!
   * shared v11.1.4
   * (c) 2025 kazuya kawaguchi
   * Released under the MIT License.
